f8efc9fa3058bb9fe9fb51c9d1456d7f8c104657
Ramses Angles <ramses.angles@hey.com>
2025-03-07 15:17:04 -0600

    setup api and dashboard ui

---
+++ app/api/chat/stream/route.ts
@@ -0,0 +1,71 @@
+import { api } from "@/convex/_generated/api";
+import { getConvexClient } from "@/lib/convex";
+import { ChatRequestBody, StreamMessage, StreamMessageType } from "@/lib/types";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+function sendSSEMessage(
+  writer: WritableStreamDefaultWriter<Uint8Array>,
+  data: StreamMessage,
+) {
+  const encoder = new TextEncoder();
+  return writer.write(
+    encoder.encode(
+      `${SSE_DATA_PREFIX}${JSON.stringify(data)}${SSE_LINE_DELIMITER}}`,
+    ),
+  );
+}
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = (await req.json()) as ChatRequestBody;
+    const { messages, newMessage, chatId } = body;
+
+    const convex = getConvexClient();
+    const stream = new TransformStream({}, { highWaterMark: 1024 });
+    const writer = stream.writable.getWriter();
+
+    const response = new Response(stream.readable, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        // "Cache-Control": "no-cache, no-transform",
+        Connection: "keep-alive",
+        "X-Accel-Buffering": "no",
+      },
+    });
+
+    const startStream = async () => {
+      try {
+        await sendSSEMessage(writer, { type: StreamMessageType.Connected });
+        await convex.mutation(api.messages.send, {
+          chatId,
+          content: newMessage,
+        });
+      } catch (error) {
+        console.error("Error in chat API:", error);
+        return NextResponse.json(
+          {
+            error: "Failed to process request",
+          } as const,
+          { status: 500 },
+        );
+      }
+    };
+    startStream();
+
+    return response;
+  } catch (error) {
+    console.error("Error in chat API:", error);
+    return NextResponse.json(
+      {
+        error: "Failed to process request",
+      } as const,
+      { status: 500 },
+    );
+  }
+}
